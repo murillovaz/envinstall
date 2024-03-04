78f502276c6a7e0258224f26760307e0be091a4a
murillovaz <murillovaz@murillovaz.murillovaz>
2024-03-04 12:19:49 -0500

    test: experimenting with bin path

---
+++ install.sh
@@ -0,0 +1,5 @@
+ENVINSTALL_DIR="/usr/local/bin" && \
+sudo wget https://raw.githubusercontent.com/murillovaz/envinstall/main/scripts/envinstall_log.sh -P $ENVINSTALL_DIR && \
+sudo wget https://raw.githubusercontent.com/murillovaz/envinstall/main/scripts/envinstall_programs.sh -P $ENVINSTALL_DIR && \
+sudo wget https://raw.githubusercontent.com/murillovaz/envinstall/main/scripts/envinstall_repositories.sh -P $ENVINSTALL_DIR && \
+sudo wget https://raw.githubusercontent.com/murillovaz/envinstall/main/scripts/envinstall.sh -P $ENVINSTALL_DIR
