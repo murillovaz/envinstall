c2ca8dcffdeea544f5287e3b557d93587508abb9
Murillo Vaz <murillo_vaz@live.com>
2025-03-01 21:45:02 -0500

    fix: set a fixed version for node in nvm install

--- scripts/envinstall_programs.sh
+++ scripts/envinstall_programs.sh
@@ -70,7 +70,7 @@ __install_node__() {
     wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
     export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-    nvm install --latest-npm --default
+    nvm install v23.9.0 
 }
 
 __install_btop__() {
